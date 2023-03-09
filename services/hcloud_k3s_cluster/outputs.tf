427164f51de5441cbea6f80548686b93dceb1331
Jens Bouma <account@jensbouma.nl>
2023-03-09 20:14:41 +0100

    change to local adress for kubeconfig

--- services/hcloud_k3s_cluster/outputs.tf
+++ services/hcloud_k3s_cluster/outputs.tf
@@ -5,7 +5,8 @@ output "private_key" {
 
 output "kubeconfig" {
   value = {
-    host                   = "https://${hcloud_server.node[values({ for key, val in var.nodes : key => key if val.k3s_type == "master" })[0]].ipv4_address}:6443"
+    /* host                   = "https://${hcloud_server.node[values({ for key, val in var.nodes : key => key if val.k3s_type == "master" })[0]].ipv4_address}:6443" */
+    host                   = "https://${values({ for key, val in var.nodes : key => key if val.k3s_type == "master" }).private_ip}:6443"   
     cluster_ca_certificate = local.certificates_by_type["server-ca"]
     client_certificate     = tls_locally_signed_cert.master_user[0].cert_pem
     client_key             = tls_private_key.master_user[0].private_key_pem
