e53dab413774b233a1e49238ef44ff9a8be09e46
Jens Bouma <account@jensbouma.nl>
2023-03-21 15:06:58 +0100

    s3 target name to variable

--- IaaS/hcloud_k3s_cluster/node.tf
+++ IaaS/hcloud_k3s_cluster/node.tf
@@ -12,7 +12,7 @@ data "template_file" "cloud-init" {
       subnet_ip_range    = var.network.subnet_ip_range
       k3s_token          = random_string.k3s_token.result
       s3_bucket          = var.s3_bucket
-      s3_bucket_name     = "${var.cluster}-etcd-backup"
+      /* s3_bucket_name     = "${var.cluster}-etcd-backup" */
       certificates_files = local.certificates_files
     }
   )
