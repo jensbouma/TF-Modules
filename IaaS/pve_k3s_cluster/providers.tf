0a429056a03425c0ec9bce37a0068eaa12d602cd
Jens Bouma <account@jensbouma.nl>
2023-06-06 13:21:21 +0200

    change 2

--- IaaS/pve_k3s_cluster/providers.tf
+++ IaaS/pve_k3s_cluster/providers.tf
@@ -1,9 +1,6 @@
 terraform {
   cloud {
     organization = "jensbouma"
-    workspaces {
-      name = "onprem_staging_cluster"
-    }
     required_providers {
      proxmox = {
       source  = "telmate/proxmox"
@@ -11,11 +8,4 @@ terraform {
      }
     }
   }
-}
-
-provider "proxmox" {
-    pm_tls_insecure = true
-    pm_api_url = var.PM_API_URL
-    pm_api_token_id = var.PM_API_TOKEN_ID
-    pm_api_token_secret = var.PM_API_TOKEN_SECRET
 }
